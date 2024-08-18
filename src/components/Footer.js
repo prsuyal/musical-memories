1337e3aabecb002549668e7370074e0ac0739abb
pranshu suyal <suyalpranshu@gmail.com>
2024-08-18 17:15:03 -0400

    setup

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GmailIconBlack from '../assets/images/icons8-gmail-black.svg';
+import GmailIcon from '../assets/images/icons8-gmail.svg';
+import TikTokIcon from '../assets/images/icons8-tiktok.svg';
+import TikTokIconBlack from '../assets/images/icons8-tiktok-black.svg';
+import InstagramIcon from '../assets/images/icons8-instagram.svg';
+import InstagramIconBlack from '../assets/images/icons8-instagram-black.svg';
+import DiscordIcon from '../assets/images/icons8-discord.svg';
+import DiscordIconBlack from '../assets/images/icons8-discord-black.svg';
+
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AppDevelopment from '../assets/images/app-development.svg';
+import Competitions from '../assets/images/competitions.svg';
+import ContactMusicIcon from '../assets/images/contact-music-icon.svg';
+import ContentCreation from '../assets/images/content-creation.svg';
+import Mail from '../assets/images/icons8-gmail.svg';
+import MailBlack from '../assets/images/icons8-gmail-black.svg';
+import Guitar from '../assets/images/guitar.svg';
+import MusicIcons from '../assets/images/music-icons.svg';
+import Performances from '../assets/images/performances.svg';
+import Tutoring from '../assets/images/tutoring.svg';
+import Workshops from '../assets/images/workshops.svg';
+
+function Home() {
+  return (
+    <div className="Home">
+        <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default Home;
